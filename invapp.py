b09b3927d83205aedf29e3906918cfa8c7f43dab
Hormoz Sohrabi <hormozsohrabi@gmail.com>
2024-02-06 09:38:17 +0330

    Update invapp.py

--- invapp.py
+++ invapp.py
@@ -2,6 +2,8 @@ species = []
 dbh=[]
 stemnumnber = []
 ans = 'y'
+ans  = str(input('do you want to add a record? y/n'))
+
 while ans=='y':
     sp = str(input('scientific name of the species?'))
     d= float(input('diameter at breast hight in cm?'))
@@ -9,7 +11,6 @@ while ans=='y':
     species.append(sp)
     dbh.append(d)
     stemnumnber.append(st)
-    ans  = str(input('do you want to add another record? y/n'))
     if ans =='n':
         break 
 print(species,dbh,stemnumnber)
